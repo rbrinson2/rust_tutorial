68fd50172a94f62382a78530838116d534be1381
rbrinson2 <rbrinson2@gmail.com>
2024-01-02 10:05:30 -0500

    Added struct tuples

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -6,6 +6,9 @@ struct User {
     sign_in_count: u64
 }
 
+struct Color (i32, i32, i32);
+struct Point (i32, i32, i32);
+
 fn main() {
     let mut user1 = User {
         active: true,
@@ -22,6 +25,9 @@ fn main() {
         email: String::from("user3@email.com"),
         ..user1
     };
+
+    let black = Color(0, 0, 0);
+    let origin = Point(0, 0, 0);
 
 }
 
--- structs/target/debug/deps/structs-e2cfe53b714ab164.d
+++ structs/target/debug/deps/structs-e2cfe53b714ab164.d
@@ -1,5 +1,5 @@
-C:\Users\rbrin\OneDrive\Repos\Rust\rust_tutorial\structs\target\debug\deps\libstructs-e2cfe53b714ab164.rmeta: src\main.rs
+C:\Users\rbrin\Documents\GitHub\rust_tutorial\structs\target\debug\deps\libstructs-e2cfe53b714ab164.rmeta: src\main.rs
 
-C:\Users\rbrin\OneDrive\Repos\Rust\rust_tutorial\structs\target\debug\deps\structs-e2cfe53b714ab164.d: src\main.rs
+C:\Users\rbrin\Documents\GitHub\rust_tutorial\structs\target\debug\deps\structs-e2cfe53b714ab164.d: src\main.rs
 
 src\main.rs:
